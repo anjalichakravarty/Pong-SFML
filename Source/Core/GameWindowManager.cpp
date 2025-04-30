368afa6341a10841802c3b25bbebe97ca2c69ea3
anjalichakravarty <anjalichakravarty9@gmail.com>
2025-04-30 14:27:27 +0530

    customize

--- Source/Core/GameWindowManager.cpp
+++ Source/Core/GameWindowManager.cpp
@@ -13,8 +13,9 @@ namespace Core {
 	{
 		//Creating the window with specified dimensions and title
 		game_window->create(
-			VideoMode(game_window_width, game_window_height),
-			game_title
+			VideoMode::getDesktopMode(),
+			game_title,
+			Style::Fullscreen
 		);
 	}
 
@@ -27,7 +28,13 @@ namespace Core {
 	void GameWindowManager::render()
 	{
 		//This will handle all the drawing operations
-		//It'll be used in future lessons
+		//Clear window with orange color(R:200, G:50, B:50, A:255)
+		game_window->clear(Color(200, 50, 50, 255));
+
+		//draw shapes, sprites
+
+		//Display the changes
+		game_window->display();
 	}
 
 	RenderWindow* GameWindowManager::getGameWindow()
